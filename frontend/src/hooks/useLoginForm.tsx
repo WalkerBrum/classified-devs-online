2fb59a79e8096afd3c93165e2cf2344f34792594
Walker Lobato <walker.b.lobato@gmail.com>
2023-12-14 16:20:50 -0300

    feat: add login in onSubmit

--- frontend/src/hooks/useLoginForm.tsx
+++ frontend/src/hooks/useLoginForm.tsx
@@ -1,7 +1,12 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { userLoginSchema } from '@schemas/userLogin';
+import { AppError } from '@utils/AppError';
+import { usersGetAll } from '@storage/users/usersGetALL';
+import { AuthNavigatorRoutesProps } from '@routes/app.routes';
 
 type LoginDataForm = {
   email: string;
@@ -14,9 +19,32 @@ export const useLoginForm = () => {
   const { control, handleSubmit, formState: { errors } } = useForm<LoginDataForm>({
     resolver: yupResolver(schema)
   });
+  
+  const { navigate } = useNavigation<AuthNavigatorRoutesProps>();
 
-  const onSubmit = (data: LoginDataForm) => {
-    console.log(data);
+  const onSubmit = async (data: LoginDataForm) => {
+    try {
+      const usersRegister = await usersGetAll();
+
+      usersRegister.map(user => {
+        if (data.email === user.email) {
+          if (data.password === user.password) {
+            return navigate('classified')
+          } else {
+            Alert.alert('Login', 'E-mail ou senha incorretos!')
+          }
+        } 
+      });
+
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Usuário', error.message);
+
+      } else {
+        Alert.alert('Login', 'Não foi possível fazer o login');
+        console.log(error);
+      }
+    }
   }
   
   return {
