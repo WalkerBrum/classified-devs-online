e48b878ddcbcbe1461048d839a2f0513b5c80f76
My Name <walker.b.lobato@gmail.com>
2023-12-21 23:13:19 -0300

    adjust style of the NoneRegister component and add your test

--- frontend/src/components/NoneRegister/index.tsx
+++ frontend/src/components/NoneRegister/index.tsx
@@ -1,7 +1,9 @@
 import {  Center, Heading } from "native-base"
 
 export const NoneRegister = () => {
-  <Center flex={1}>
-    <Heading fontFamily="heading" fontSize="lg" color="gray.700">Nenhum anúncio cadastrado</Heading>
-  </Center>
+  return (
+    <Center pt={130}>
+      <Heading fontFamily="body" fontSize="md" color="gray.300">Nenhum anúncio cadastrado</Heading>
+    </Center>
+  )
 }
