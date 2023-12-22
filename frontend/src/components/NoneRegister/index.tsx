aed8c3fac968328435ac102359ada43d258598ab
My Name <walker.b.lobato@gmail.com>
2023-12-21 22:33:05 -0300

    add component NoneRegister

---
+++ frontend/src/components/NoneRegister/index.tsx
@@ -0,0 +1,7 @@
+import {  Center, Heading } from "native-base"
+
+export const NoneRegister = () => {
+  <Center flex={1}>
+    <Heading fontFamily="heading" fontSize="lg" color="gray.700">Nenhum anúncio cadastrado</Heading>
+  </Center>
+}
